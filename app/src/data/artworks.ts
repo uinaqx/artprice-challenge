36c8bb6e33dbca82f9bb4284ded15183503c18a5
uinaqx <uinaqx@example.com>
2026-01-30 23:14:31 +0800

    添加31张本地图片，移除外部图片链接

--- app/src/data/artworks.ts
+++ app/src/data/artworks.ts
@@ -21,7 +21,7 @@ export const artworks: Artwork[] = [
     year: "1914-1916",
     price: 236400000,
     type: "油画",
-    image: "https://kimi-web-img.moonshot.cn/img/www.caproasia.com/6c5b07eaa3abc0c1fb0087c0181e29394b6d4dbf.jpg",
+    image: "/images/1.jpg",
     description: "维也纳分离派黄金时代的巅峰之作，克林姆特晚期肖像画的代表作。"
   },
   {
@@ -33,7 +33,7 @@ export const artworks: Artwork[] = [
     year: "1907",
     price: 86000000,
     type: "油画",
-    image: "https://kimi-web-img.moonshot.cn/img/e3.365dm.com/edb1a9d4804932a90fb888e08078ff23f222d2db.jpg",
+    image: "/images/2.jpg",
     description: "克林姆特风景画第二高价，创作于艺术家与缪斯埃米莉·芙洛格共度的夏日之旅。"
   },
   {
@@ -45,7 +45,7 @@ export const artworks: Artwork[] = [
     year: "1887",
     price: 62700000,
     type: "油画",
-    image: "https://kimi-web-img.moonshot.cn/img/s.turbifycdn.com/7599f165f682aec866e75d299442ff548bba2636.png",
+    image: "/images/3.jpg",
     description: "梵高静物画拍卖纪录，深受日本版画影响，预示了梵高成熟时期的独特风格。"
   },
   {
@@ -57,7 +57,7 @@ export const artworks: Artwork[] = [
     year: "1958",
     price: 62160000,
     type: "油画",
-    image: "https://upload.wikimedia.org/wikipedia/en/5/5c/No._61_%28Rust_and_Blue%29.jpg",
+    image: "/images/4.jpg",
     description: "罗斯科艺术生涯关键转折期代表作，刷新佳士得线上出价纪录。"
   },
   {
@@ -69,7 +69,7 @@ export const artworks: Artwork[] = [
     year: "1940",
     price: 54660000,
     type: "油画",
-    image: "https://kimi-web-img.moonshot.cn/img/www.fridakahlo.org/fa26300f975715d33a465ea56b79494d7c3103e7.jpg",
+    image: "/images/5.jpg",
     description: "创下女性艺术家、拉丁美洲艺术家作品全球最高拍卖纪录。"
   },
   {
@@ -81,7 +81,7 @@ export const artworks: Artwork[] = [
     year: "1981",
     price: 48335000,
     type: "油画",
-    image: "https://kimi-web-img.moonshot.cn/img/news.artnet.com/c408fe9ed71826a4967e95afbe0f3a62ffa8beab.jpg",
+    image: "/images/6.jpg",
     description: "巴斯奎特里程碑个展作品，皇冠与骷髅头两大标志皆呈现于此。"
   },
   {
@@ -93,7 +93,7 @@ export const artworks: Artwork[] = [
     year: "1922",
     price: 47560000,
     type: "油画",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Piet_Mondriaan_-_Composition_A_-_Google_Art_Project.jpg/800px-Piet_Mondriaan_-_Composition_A_-_Google_Art_Project.jpg",
+    image: "/images/7.jpg",
     description: "新造型主义代表作，以精密的黑色网格分隔画布，构建动态平衡。"
   },
   {
@@ -105,7 +105,7 @@ export const artworks: Artwork[] = [
     year: "1932",
     price: 45485000,
     type: "油画",
-    image: "https://upload.wikimedia.org/wikipedia/en/7/74/PicassoWeepingWoman.jpg",
+    image: "/images/8.jpg",
     description: "毕加索重回古典'阅读中的女人'主题的代表作，缪斯玛丽·特雷斯为灵感源泉。"
   },
   {
@@ -117,7 +117,7 @@ export const artworks: Artwork[] = [
     year: "1907",
     price: 45485000,
     type: "油画",
-    image: "https://upload.wikimedia.org/wikipedia/commons/a/aa/Claude_Monet_-_Water_Lilies_-_1906%2C_Ryerson.jpg",
+    image: "/images/9.jpg",
     description: "莫奈晚年对光影与色彩进行艺术实验的巅峰探索，尽显大师创作成熟期的艺术造诣。"
   },
   {
@@ -129,7 +129,7 @@ export const artworks: Artwork[] = [
     year: "1968",
     price: 44335000,
     type: "油画",
-    image: "https://upload.wikimedia.org/wikipedia/en/3/3c/A_Bigger_Splash.jpg",
+    image: "/images/10.jpg",
     description: "霍克尼笔下仅存的七幅大型双人肖像画作之一，极具戏剧张力的罕见力作。"
   },
   {
@@ -141,7 +141,7 @@ export const artworks: Artwork[] = [
     year: "1969",
     price: 14230000,
     type: "国画",
-    image: "https://kimi-web-img.moonshot.cn/img/img2.chinadaily.com.cn/8cb0b172f0e19eacef5c820062eb662a51571da7.jpeg",
+    image: "/images/11.jpg",
     description: "张大千泼墨泼彩山水代表作，展现了艺术家晚年泼墨泼彩的成熟技法。"
   },
   {
@@ -153,7 +153,7 @@ export const artworks: Artwork[] = [
     year: "1940s",
     price: 14230000,
     type: "国画",
-    image: "https://kimi-web-img.moonshot.cn/img/video.cgtn.com/ce38f700d6478805df798b745ed72c58c737736b.jpg",
+    image: "/images/12.jpg",
     description: "齐白石册页精品，融合了民间艺术与传统文人画的精髓。"
   },
   {
@@ -165,7 +165,7 @@ export const artworks: Artwork[] = [
     year: "1690s",
     price: 7100000,
     type: "国画",
-    image: "https://kimi-web-img.moonshot.cn/img/www.chinaonlinemuseum.com/5451cbdcc88f9ae84da6b6af3d971c396455095d.jpg",
+    image: "/images/13.jpg",
     description: "八大山人晚年精品，画面简洁空灵，寄托了画家孤傲清高的情怀。"
   },
   {
@@ -177,7 +177,7 @@ export const artworks: Artwork[] = [
     year: "c.1500",
     price: 450300000,
     type: "油画",
-    image: "https://upload.wikimedia.org/wikipedia/commons/5/5c/Leonardo_da_Vinci%2C_Salvator_Mundi%2C_c.1500.jpg",
+    image: "/images/14.jpg",
     description: "拍卖史上最昂贵的艺术品之一，被誉为'男版蒙娜丽莎'。"
   },
   {
@@ -189,7 +189,7 @@ export const artworks: Artwork[] = [
     year: "1892-1893",
     price: 250000000,
     type: "油画",
-    image: "https://upload.wikimedia.org/wikipedia/commons/0/0e/Les_Joueurs_de_cartes%2C_par_Paul_C%C3%A9zanne.jpg",
+    image: "/images/15.jpg",
     description: "塞尚系列作品中最著名的一幅，画面构图严谨，色彩沉稳。"
   },
   {
@@ -201,7 +201,7 @@ export const artworks: Artwork[] = [
     year: "1892",
     price: 210000000,
     type: "油画",
-    image: "https://upload.wikimedia.org/wikipedia/commons/2/2e/Paul_Gauguin_-_Nafea_Faa_Ipoipo%3F_%28When_Will_You_Marry%3F%29_-_Google_Art_Project.jpg",
+    image: "/images/16.jpg",
     description: "高更大溪地时期的代表作，描绘了南太平洋岛国的风土人情。"
   },
   {
@@ -213,7 +213,7 @@ export const artworks: Artwork[] = [
     year: "1994-2000",
     price: 58400000,
     type: "雕塑",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Balloon_Dog_%28Orange%29_by_Jeff_Koons.jpg/800px-Balloon_Dog_%28Orange%29_by_Jeff_Koons.jpg",
+    image: "/images/17.jpg",
     description: "杰夫·昆斯标志性气球动物系列，以不锈钢材质呈现童年记忆。"
   },
   {
@@ -225,7 +225,7 @@ export const artworks: Artwork[] = [
     year: "1961",
     price: 104300000,
     type: "雕塑",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Alberto_Giacometti_-_Walking_Man_I.jpg/800px-Alberto_Giacometti_-_Walking_Man_I.jpg",
+    image: "/images/18.jpg",
     description: "贾科梅蒂细长人形雕塑的代表作，表现了人类生存的孤独与脆弱。"
   },
   {
@@ -237,7 +237,7 @@ export const artworks: Artwork[] = [
     year: "1895",
     price: 119900000,
     type: "油画",
-    image: "https://upload.wikimedia.org/wikipedia/commons/c/c5/Edvard_Munch%2C_1893%2C_The_Scream%2C_oil%2C_tempera_and_pastel_on_cardboard%2C_91_x_73_cm%2C_National_Gallery_of_Norway.jpg",
+    image: "/images/19.jpg",
     description: "表现主义最具标志性的作品，描绘了人类面对存在焦虑时的呐喊。"
   },
   {
@@ -249,7 +249,7 @@ export const artworks: Artwork[] = [
     year: "1965",
     price: 8500000,
     type: "国画",
-    image: "https://kimi-web-img.moonshot.cn/img/collectionapi.metmuseum.org/429d5428d8c3328e9ce9a769c9b42eaa03c9dce4",
+    image: "/images/20.jpg",
     description: "张大千泼墨荷花精品，现藏于大都会艺术博物馆。"
   },
   {
@@ -261,7 +261,7 @@ export const artworks: Artwork[] = [
     year: "1947",
     price: 141000000,
     type: "雕塑",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Alberto_Giacometti_-_Walking_Man_I.jpg/800px-Alberto_Giacometti_-_Walking_Man_I.jpg",
+    image: "/images/21.jpg",
     description: "全球雕塑拍卖最高纪录保持者，手指指向前方的瘦长人形，充满存在主义张力。"
   },
   {
@@ -273,7 +273,7 @@ export const artworks: Artwork[] = [
     year: "1986",
     price: 91075000,
     type: "雕塑",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Jeff_Koons_Rabbit.jpg/800px-Jeff_Koons_Rabbit.jpg",
+    image: "/images/22.jpg",
     description: "不锈钢镜面气球兔，杰夫·昆斯庆典系列代表作，创下在世艺术家拍卖世界纪录。"
   },
   {
@@ -285,7 +285,7 @@ export const artworks: Artwork[] = [
     year: "1995-2004",
     price: 33682500,
     type: "雕塑",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Jeff_Koons_Tulips.jpg/800px-Jeff_Koons_Tulips.jpg",
+    image: "/images/23.jpg",
     description: "重达3吨的不锈钢镜面气球花束，糖果色系金属质感，庆典系列最复杂的作品之一。"
   },
   {
@@ -297,7 +297,7 @@ export const artworks: Artwork[] = [
     year: "2019",
     price: 6240000,
     type: "装置",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Maurizio_Cattelan_Comedian.jpg/800px-Maurizio_Cattelan_Comedian.jpg",
+    image: "/images/24.jpg",
     description: "一根用胶带贴在墙上的香蕉，概念艺术代表作，买家获得真实性证书和更换香蕉的权利。"
   },
   {
@@ -309,7 +309,7 @@ export const artworks: Artwork[] = [
     year: "2016",
     price: 12100000,
     type: "雕塑",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/8e/Maurizio_Cattelan_America.jpg/800px-Maurizio_Cattelan_America.jpg",
+    image: "/images/25.jpg",
     description: "18克拉黄金打造的功能性马桶，重达220磅，使用者可直接在上面如厕。"
   },
   {
@@ -321,7 +321,7 @@ export const artworks: Artwork[] = [
     year: "1991",
     price: 12000000,
     type: "装置",
-    image: "https://upload.wikimedia.org/wikipedia/en/6/6f/Damien_Hirst_Shark_1991.jpg",
+    image: "/images/26.jpg",
     description: "4米长虎鲨浸泡在甲醛溶液中，YBA年轻英国艺术家运动最具标志性的作品。"
   },
   {
@@ -333,7 +333,7 @@ export const artworks: Artwork[] = [
     year: "2007",
     price: 100000000,
     type: "雕塑",
-    image: "https://upload.wikimedia.org/wikipedia/en/3/30/For_the_Love_of_God.jpg",
+    image: "/images/27.jpg",
     description: "真人大小铂金骷髅，镶嵌8601颗钻石，既是死亡警示也是对过度消费的批判。"
   },
   {
@@ -345,7 +345,7 @@ export const artworks: Artwork[] = [
     year: "2014",
     price: 8000000,
     type: "雕塑",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Yayoi_Kusama_Pumpkin.jpg/800px-Yayoi_Kusama_Pumpkin.jpg",
+    image: "/images/28.jpg",
     description: "逾2米高的青铜南瓜雕塑，草间弥生最具标志性的波点南瓜巨型版本。"
   },
   {
@@ -357,7 +357,7 @@ export const artworks: Artwork[] = [
     year: "2018",
     price: 3300000,
     type: "装置",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Yayoi_Kusama_Infinity_Mirror_Room.jpg/800px-Yayoi_Kusama_Infinity_Mirror_Room.jpg",
+    image: "/images/29.jpg",
     description: "高达2米的无限镜屋装置，LED光影与镜面创造出无限延伸的宇宙空间。"
   },
   {
@@ -369,7 +369,7 @@ export const artworks: Artwork[] = [
     year: "2018/2021",
     price: 25450000,
     type: "油画",
-    image: "https://upload.wikimedia.org/wikipedia/en/6/6d/Love_is_in_the_Bin.jpg",
+    image: "/images/30.jpg",
     description: "《气球女孩》原作在拍卖落槌后被画框内隐藏碎纸机切碎，成为新的艺术作品。"
   },
   {
@@ -381,7 +381,7 @@ export const artworks: Artwork[] = [
     year: "1968-1969",
     price: 31400000,
     type: "雕塑",
-    image: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Francois_Xavier_Lalanne_Hippopotame_Bar.jpg/800px-Francois_Xavier_Lalanne_Hippopotame_Bar.jpg",
+    image: "/images/31.jpg",
     description: "等身大铜制河马雕塑，打开后露出内部酒吧空间，功能性与雕塑艺术的完美结合。"
   }
 ];
